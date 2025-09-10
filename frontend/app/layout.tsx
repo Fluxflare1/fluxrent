9bae94dcec2c46a71fad6a3df88417707165ac47
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-10 23:24:49 +0100

    Create layout.tsx

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import Sidebar from '../components/layout/Sidebar'
+import Topbar from '../components/layout/Topbar'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="flex h-screen bg-gray-50">
+        <Sidebar />
+        <main className="flex-1 flex flex-col">
+          <Topbar />
+          <div className="p-6">{children}</div>
+        </main>
+      </body>
+    </html>
+  )
+}
