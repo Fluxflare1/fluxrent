7ca9738142e8492402622601d89359246c9cf8e0
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-10 23:27:52 +0100

    Create page.tsx

---
+++ frontend/app/admin/tenants/new/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import TenantForm from "../../../../components/forms/TenantForm"
+
+export default function NewTenantPage() {
+  return (
+    <div>
+      <h1 className="text-xl font-bold mb-4">Add New Tenant</h1>
+      <TenantForm />
+    </div>
+  )
+}
