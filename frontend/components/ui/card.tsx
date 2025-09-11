df1fd5c2330f93496df25054c0e1e6101b721fd7
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-11 15:10:58 +0100

    Create card.tsx

---
+++ frontend/components/ui/card.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={`rounded-lg border bg-white shadow-sm ${className}`}
+    {...props}
+  />
+))
+Card.displayName = 'Card'
+
+export const CardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={`flex flex-col space-y-1.5 p-6 ${className}`}
+    {...props}
+  />
+))
+CardHeader.displayName = 'CardHeader'
+
+export const CardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={`p-6 pt-0 ${className}`} {...props} />
+))
+CardContent.displayName = 'CardContent'
