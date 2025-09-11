ca8bcd6d17c21aa5affba49f5fbe8b25b3245a26
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-11 02:10:40 +0100

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '../styles/globals.css'
 import React from 'react'
 import Sidebar from '../components/layout/Sidebar'
 import Topbar from '../components/layout/Topbar'
