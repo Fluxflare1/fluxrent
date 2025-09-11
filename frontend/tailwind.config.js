fe81ab737c35695ddc093bc9d7c13d8323630955
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-11 01:04:22 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: { extend: {} },
-  plugins: []
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
