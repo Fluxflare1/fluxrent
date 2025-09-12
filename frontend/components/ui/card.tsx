128d1f0fbad94a90b9940db8ef9ea7beb480e3d1
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-12 06:20:11 +0100

    Update card.tsx

--- frontend/components/ui/card.tsx
+++ frontend/components/ui/card.tsx
@@ -1,35 +1,49 @@
-// frontend/components/ui/card.tsx
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-function Card({ className, ...props }: CardProps) {
-  return (
+const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className, ...props }, ref) => (
     <div
+      ref={ref}
       className={cn(
         "rounded-xl border border-gray-200 bg-white text-gray-900 shadow-soft",
         className
       )}
       {...props}
     />
-  );
-}
+  )
+);
+Card.displayName = "Card";
 
-function CardHeader({ className, ...props }: CardProps) {
-  return (
-    <div className={cn("px-4 py-3 border-b border-gray-100", className)} {...props} />
-  );
-}
+const CardHeader = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn("px-4 py-3 border-b border-gray-100", className)}
+      {...props}
+    />
+  )
+);
+CardHeader.displayName = "CardHeader";
 
-function CardContent({ className, ...props }: CardProps) {
-  return <div className={cn("px-4 py-3", className)} {...props} />;
-}
+const CardContent = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className, ...props }, ref) => (
+    <div className={cn("px-4 py-3", className)} {...props} />
+  )
+);
+CardContent.displayName = "CardContent";
 
-function CardFooter({ className, ...props }: CardProps) {
-  return (
-    <div className={cn("px-4 py-3 border-t border-gray-100", className)} {...props} />
-  );
-}
+const CardFooter = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn("px-4 py-3 border-t border-gray-100", className)}
+      {...props}
+    />
+  )
+);
+CardFooter.displayName = "CardFooter";
 
 export { Card, CardHeader, CardContent, CardFooter };
