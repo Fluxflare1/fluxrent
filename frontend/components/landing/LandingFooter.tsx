22386b22207c7fbcc331ea7b805ea747cdb68ed2
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-12 23:22:14 +0100

    Create LandingFooter.tsx

---
+++ frontend/components/landing/LandingFooter.tsx
@@ -0,0 +1,9 @@
+export default function LandingFooter() {
+  return (
+    <footer className="bg-blue-900 text-white px-8 py-8 text-center">
+      <p className="text-sm">
+        © {new Date().getFullYear()} Whitepace. All rights reserved.
+      </p>
+    </footer>
+  );
+}
