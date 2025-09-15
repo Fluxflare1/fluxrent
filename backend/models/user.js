29ee17f047a1fca60f018e6877181150feebd40c
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-15 23:20:23 +0100

    Create user.js

---
+++ backend/models/user.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+const bcrypt = require("bcrypt");
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define associations here if needed
+    }
+  }
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      role: {
+        type: DataTypes.STRING,
+        defaultValue: "TENANT"
+      }
+    },
+    {
+      sequelize,
+      modelName: "User",
+      hooks: {
+        beforeCreate: async (user) => {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      }
+    }
+  );
+  return User;
+};
