d9e3b0a94c7410cfe555b634fddb3e279f539640
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-17 00:07:47 +0100

    Delete backend directory

--- backend/models/index.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require("../sequelize");
-const User = require("./user");
-
-async function initModels() {
-  await sequelize.sync({ alter: true });
-}
-
-module.exports = { sequelize, User, initModels };
