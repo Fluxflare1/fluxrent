19aeb6a898d7ec875b865ba7b2ef685668bbf29f
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-17 10:36:35 +0100

    Update asgi.py

--- backend/fluxrent/asgi.py
+++ backend/fluxrent/asgi.py
@@ -1,4 +1,6 @@
 import os
 from django.core.asgi import get_asgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fluxrent.settings')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fluxrent.settings")
+
 application = get_asgi_application()
