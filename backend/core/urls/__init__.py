560a8bfdcc189db4a698ad0140703c737cc87d68
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-17 10:57:40 +0100

    Create __init__.py

---
+++ backend/core/urls/__init__.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("auth/", include("core.urls.auth")),   # Phase 1 Auth
+    path("", include("core.urls.admin_panel")), # Phase 2 Admin Panel
+]
