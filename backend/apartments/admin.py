b9de1a9ec7d448e982ec4dec80f2bb800c9222a6
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-18 15:51:19 +0100

    Update admin.py

--- backend/apartments/admin.py
+++ backend/apartments/admin.py
@@ -1,7 +1,9 @@
+# backend/apartments/admin.py
 from django.contrib import admin
 from .models import Apartment
 
 @admin.register(Apartment)
 class ApartmentAdmin(admin.ModelAdmin):
-    list_display = ("id", "uid", "property", "number", "rent_amount")
-    search_fields = ("uid", "number")
+    list_display = ("uid", "property", "number", "floor", "bedrooms", "is_occupied", "tenant")
+    search_fields = ("uid", "number", "property__uid", "property__name")
+    list_filter = ("is_occupied", "bedrooms", "floor", "created_at")
