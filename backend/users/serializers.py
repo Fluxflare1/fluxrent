d15d20833c8d6ce3b09ee8fea5c511f0b78476c8
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-18 12:53:16 +0100

    Update serializers.py

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -7,19 +7,25 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "uid", "username", "email", "first_name", "last_name", "role", "phone", "is_active", "date_joined", "dva"]
-        read_only_fields = ["id", "uid", "date_joined", "dva"]
+        fields = ("id", "email", "first_name", "last_name", "phone", "role", "uid", "dva", "is_active", "is_staff", "metadata")
+        read_only_fields = ("id", "uid", "dva", "is_staff")
 
 class UserCreateSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8, required=True)
+    password = serializers.CharField(write_only=True, required=True, min_length=8)
 
     class Meta:
         model = User
-        fields = ["username", "email", "first_name", "last_name", "password", "phone"]
+        fields = ("id", "email", "first_name", "last_name", "phone", "password", "role")
 
     def create(self, validated_data):
         password = validated_data.pop("password")
-        user = User(**validated_data)
+        user = User.objects.create(**validated_data)
         user.set_password(password)
         user.save()
         return user
+
+class UserAdminSerializer(UserSerializer):
+    # Admin serializer exposes extra fields
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + ("is_staff", "is_superuser", "date_joined")
+        read_only_fields = ("uid",)
