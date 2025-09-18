c5297402a1a7408d57c09c0f1516cac57b23145b
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-18 10:30:46 +0100

    Update manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -7,7 +7,7 @@ def main():
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise
+        raise ImportError("Django not installed") from exc
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
