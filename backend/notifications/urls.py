31702f3be0a0bf377e7b93c64d514894afc06344
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-18 12:10:47 +0100

    Create urls.py

---
+++ backend/notifications/urls.py
@@ -0,0 +1,7 @@
+# backend/notifications/urls.py
+from rest_framework.routers import DefaultRouter
+from .views import NotificationViewSet
+
+router = DefaultRouter()
+router.register(r"", NotificationViewSet, basename="notifications")
+urlpatterns = router.urls
