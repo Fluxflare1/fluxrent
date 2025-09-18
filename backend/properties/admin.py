41546366fd28d280c6f3a792142353f0dd0be0a4
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-18 15:52:15 +0100

    Update admin.py

--- backend/properties/admin.py
+++ backend/properties/admin.py
@@ -1,17 +1,9 @@
 # backend/properties/admin.py
 from django.contrib import admin
-from .models import Property, Apartment
+from .models import Property
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ("uid", "name", "owner", "state_code", "lga_code", "created_at")
-    search_fields = ("uid", "name", "address", "owner__email")
-    readonly_fields = ("uid", "created_at", "updated_at")
-    raw_id_fields = ("owner",)
-
-@admin.register(Apartment)
-class ApartmentAdmin(admin.ModelAdmin):
-    list_display = ("uid", "property", "number", "floor", "bedrooms", "rent_amount", "is_occupied")
-    search_fields = ("uid", "number", "property__uid", "property__name")
-    readonly_fields = ("uid", "created_at", "updated_at")
-    raw_id_fields = ("property", "tenant")
+    list_display = ("uid", "name", "state_code", "lga_code", "owner", "created_at")
+    search_fields = ("uid", "name", "state_code", "lga_code")
+    list_filter = ("state_code", "lga_code", "created_at")
