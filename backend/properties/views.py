96ae517a40d2870d0d81d1a9c2da7c1977dadb3e
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-19 23:39:59 +0100

    Update views.py

--- backend/properties/views.py
+++ backend/properties/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, permissions
 from .models import Property
 from apartments.models import Apartment
 from .serializers import PropertySerializer, ApartmentSerializer
-from .permissions import IsPropertyMsnagerOrReadOnly
+from .permissions import IsPropertyManagerOrReadOnly
 
 
 class PropertyViewSet(viewsets.ModelViewSet):
