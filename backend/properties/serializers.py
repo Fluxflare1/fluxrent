66483eb4f5b0c0e30f8ba249fea0a4f0b63b79cc
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-19 10:46:30 +0100

    Update serializers.py

--- backend/properties/serializers.py
+++ backend/properties/serializers.py
@@ -1,29 +1,7 @@
 # backend/properties/serializers.py
 from rest_framework import serializers
-from .models import Property, Apartment
-from django.conf import settings
-
-class PropertySerializer(serializers.ModelSerializer):
-    owner_email = serializers.EmailField(source="owner.email", read_only=True)
-
-    class Meta:
-        model = Property
-        fields = [
-            "id",
-            "uid",
-            "name",
-            "address",
-            "state_code",
-            "lga_code",
-            "street",
-            "house_no",
-            "owner",
-            "owner_email",
-            "external_id",
-            "created_at",
-            "updated_at",
-        ]
-        read_only_fields = ("uid", "created_at", "updated_at", "owner_email")
+from .models import Property
+from apartments.models import Apartment
 
 class ApartmentSerializer(serializers.ModelSerializer):
     property_uid = serializers.CharField(source="property.uid", read_only=True)
@@ -46,4 +24,31 @@ class ApartmentSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
         ]
-        read_only_fields = ("uid", "created_at", "updated_at", "property_uid", "tenant_email")
+        read_only_fields = ("id", "uid", "created_at", "updated_at", "property_uid", "tenant_email")
+
+
+class PropertySerializer(serializers.ModelSerializer):
+    owner_email = serializers.EmailField(source="owner.email", read_only=True)
+    apartments = ApartmentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Property
+        fields = [
+            "id",
+            "uid",
+            "name",
+            "address",
+            "state_code",
+            "lga_code",
+            "street",
+            "house_no",
+            "owner",
+            "owner_email",
+            "external_id",
+            "created_at",
+            "updated_at",
+            "apartments",
+        ]
+        read_only_fields = ("id", "uid", "created_at", "updated_at", "owner_email")
+
+
