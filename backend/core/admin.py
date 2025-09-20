5ee69462c4c6d6c2e66c7d38c8c72a23aa13955a
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-20 02:45:04 +0100

    Create admin.py

---
+++ backend/core/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = "FluxRent Admin"
+admin.site.site_title = "FluxRent Admin Portal"
+admin.site.index_title = "Welcome to FluxRent Management"
