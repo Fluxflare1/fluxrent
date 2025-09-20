9fa4854b69c1901031c1ac74c8e23192f203dc89
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-20 03:58:09 +0100

    Update admin.py

--- backend/properties/admin.py
+++ backend/properties/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Property
 
+
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "address", "city")  # replaced 'location'
-    search_fields = ("name", "address", "city")
+    list_display = ("id", "name", "address", "created_at")
+    search_fields = ("name", "address")
