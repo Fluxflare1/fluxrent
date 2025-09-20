9f591b2b2359a94c55f2c1464e3ab6fe630384fb
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-20 04:32:20 +0100

    Update settings.py

--- backend/fluxrent/settings.py
+++ backend/fluxrent/settings.py
@@ -3,3 +3,18 @@
 from core.settings import *  # noqa: F401,F403
 
 # You can overwrite project-specific values here if needed.
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],  # global templates directory
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
