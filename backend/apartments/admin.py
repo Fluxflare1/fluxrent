ecbf89a263b5ed46ee72494b4eb5546a48763b8c
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-20 03:49:37 +0100

    Update admin.py

--- backend/apartments/admin.py
+++ backend/apartments/admin.py
@@ -1,24 +1,7 @@
 from django.contrib import admin
 from .models import Apartment
-from tenants.models import TenantApartment, BondRequest
-
-
-class TenantApartmentInline(admin.TabularInline):
-    model = TenantApartment
-    extra = 0
-    autocomplete_fields = ["tenant"]
-
-
-class BondRequestInline(admin.TabularInline):
-    model = BondRequest
-    extra = 0
-    autocomplete_fields = ["tenant"]
-
 
 @admin.register(Apartment)
 class ApartmentAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "property", "created_at"]
-    search_fields = ["name"]
-    list_filter = ["created_at"]
-    autocomplete_fields = ["property"]
-    inlines = [TenantApartmentInline, BondRequestInline]
+    list_display = ("id", "property", "unit_number", "rent", "status")  # fixed
+    search_fields = ("unit_number",)
