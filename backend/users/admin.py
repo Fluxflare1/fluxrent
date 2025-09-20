543d34daf9123569ed624fe1863f4b58c495e634
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-20 03:51:40 +0100

    Update admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -4,22 +4,17 @@ from tenants.models import TenantApartment, BondRequest
 
 User = get_user_model()
 
-
 class TenantApartmentInline(admin.TabularInline):
     model = TenantApartment
+    fk_name = "tenant"  # FIX: specify FK
     extra = 0
-    autocomplete_fields = ["apartment"]
-
 
 class BondRequestInline(admin.TabularInline):
     model = BondRequest
+    fk_name = "tenant"  # FIX: specify FK
     extra = 0
-    autocomplete_fields = ["apartment"]
-
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["id", "username", "email", "is_active", "is_staff", "date_joined"]
-    list_filter = ["is_active", "is_staff", "date_joined"]
-    search_fields = ["username", "email"]
+    list_display = ("id", "username", "email", "is_staff", "is_active")
     inlines = [TenantApartmentInline, BondRequestInline]
