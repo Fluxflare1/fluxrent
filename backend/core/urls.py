97b7eba613c45d0ddf6efe6c2e6baa9f55ec94b6
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-20 03:43:17 +0100

    Update urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from .admin import custom_admin_site
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,4 +11,5 @@ urlpatterns = [
     path("api/bills/", include("bills.urls")),
     path("api/wallets/", include("wallets.urls")),
     path("api/payments/", include("payments.urls")),
+    path("admin/", custom_admin_site.urls),
 ]
