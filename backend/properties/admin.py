f9af1122ee521f7cab762b50a140df8105b41c6b
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-20 03:50:25 +0100

    Update admin.py

--- backend/properties/admin.py
+++ backend/properties/admin.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
 from .models import Property
-from apartments.models import Apartment
-
-
-class ApartmentInline(admin.TabularInline):
-    model = Apartment
-    extra = 0
-
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "location", "owner", "created_at"]
-    search_fields = ["name", "location"]
-    list_filter = ["created_at"]
-    inlines = [ApartmentInline]
+    list_display = ("id", "name", "address", "city")  # replaced 'location'
+    search_fields = ("name", "address", "city")
