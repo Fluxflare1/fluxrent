c379d0f2971e7fe8c1bc7b6f1f98b67da57a9c1b
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-20 03:13:30 +0100

    Update admin.py

--- backend/apartments/admin.py
+++ backend/apartments/admin.py
@@ -1,6 +1,13 @@
-# backend/apartments/admin.py
 from django.contrib import admin
 from .models import Apartment
+from tenants.models import TenantApartment
+
+
+class TenantApartmentInline(admin.TabularInline):
+    model = TenantApartment
+    extra = 0
+    fields = ("tenant", "bond_status", "requested_at", "activated_at")
+    readonly_fields = ("requested_at",)
 
 
 @admin.register(Apartment)
@@ -9,4 +16,4 @@ class ApartmentAdmin(admin.ModelAdmin):
     list_filter = ("is_available", "created_at")
     search_fields = ("name", "unit_number", "property__name")
     ordering = ("-created_at",)
-
+    inlines = [TenantApartmentInline]
