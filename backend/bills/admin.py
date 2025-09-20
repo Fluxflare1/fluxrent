2662ca061f5c6f6cc0a0feb33976d3bc43b606e6
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-20 04:00:12 +0100

    Update admin.py

--- backend/bills/admin.py
+++ backend/bills/admin.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 from .models import Invoice
 from payments.models import PaymentRecord
 
+
 class PaymentRecordInline(admin.TabularInline):
     model = PaymentRecord
     extra = 0
+    autocomplete_fields = ["tenant", "confirmed_by"]
+
 
 @admin.register(Invoice)
 class InvoiceAdmin(admin.ModelAdmin):
-    list_display = ("id", "tenant", "total_amount", "status", "due_date", "created_at")  # fixed
-    list_filter = ("status",)
+    list_display = ("id", "tenant_apartment", "total_amount", "status", "due_date", "created_at")
+    list_filter = ("status", "due_date")
+    search_fields = ("tenant_apartment__tenant__username", "tenant_apartment__apartment__id")
+    autocomplete_fields = ("tenant_apartment",)
     inlines = [PaymentRecordInline]
