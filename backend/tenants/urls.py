c70444bc4c5adca7db1486e13c0737424734c2c6
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-23 13:34:22 +0100

    Update urls.py

--- backend/tenants/urls.py
+++ backend/tenants/urls.py
@@ -1,13 +1,9 @@
-from rest_framework import routers
-from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TenantBondViewSet, TenantApartmentViewSet, StatementOfStayViewSet
 
-from .views import BondRequestViewSet, TenantApartmentViewSet, StatementOfStayViewSet
-
-router = routers.DefaultRouter()
-router.register(r"bond-requests", BondRequestViewSet, basename="bondrequest")
-router.register(r"tenant-apartments", TenantApartmentViewSet, basename="tenantapartment")
+router = DefaultRouter()
+router.register(r"bonds", TenantBondViewSet, basename="tenant-bond")
+router.register(r"apartments", TenantApartmentViewSet, basename="tenant-apartment")
 router.register(r"statements", StatementOfStayViewSet, basename="statement")
 
-urlpatterns = [
-    path("", include(router.urls)),
-]
+urlpatterns = router.urls
