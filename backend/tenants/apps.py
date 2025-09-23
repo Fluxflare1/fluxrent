ae243be8df06fcff0be290ea0a65d328ab709e62
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-23 10:41:29 +0100

    Create apps.py

---
+++ backend/tenants/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class TenantsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tenants"
+    verbose_name = "Tenants"
+
+    def ready(self):
+        # Import signals here if you add them later
+        pass
