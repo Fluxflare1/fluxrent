7910618d17a73d0a3d7cb8ede1701b6457668fb0
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-23 12:46:37 +0100

    Update admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,26 +1,25 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from .models import User, KYC
+from django.utils.translation import gettext_lazy as _
 
 
-class UserAdmin(BaseUserAdmin):
-    ordering = ["email"]
-    list_display = ["email", "first_name", "last_name", "role", "kyc_completed", "is_staff"]
-    search_fields = ["email", "first_name", "last_name", "phone_number"]
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
     fieldsets = (
-        (None, {"fields": ("email", "password", "uid")}),
-        ("Personal Info", {"fields": ("first_name", "last_name", "phone_number")}),
-        ("Role & KYC", {"fields": ("role", "kyc_completed")}),
-        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
-        ("Important dates", {"fields": ("last_login", "date_joined")}),
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "phone_number")}),
+        (_("Permissions"), {"fields": ("role", "is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("email", "first_name", "last_name", "phone_number", "password1", "password2", "role"),
-        }),
-    )
+    add_fieldsets = ((None, {"classes": ("wide",), "fields": ("email", "password1", "password2", "role")} ),)
+    list_display = ("email", "first_name", "last_name", "role", "is_staff", "is_superuser")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("-date_joined",)
+    readonly_fields = ("date_joined",)
 
 
-admin.site.register(User, UserAdmin)
-admin.site.register(KYC)
+@admin.register(KYC)
+class KYCAdmin(admin.ModelAdmin):
+    list_display = ("user", "verified", "verified_at", "created_at")
+    search_fields = ("user__email", "full_name")
