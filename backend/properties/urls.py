fbdc9eae31d06c8edd80759b436f6f02f3ce2e50
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-23 13:24:22 +0100

    Create urls.py

---
+++ backend/properties/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+from .views import PropertyViewSet, ApartmentViewSet
+
+router = DefaultRouter()
+router.register(r"properties", PropertyViewSet, basename="property")
+router.register(r"apartments", ApartmentViewSet, basename="apartment")
+
+urlpatterns = router.urls
