a8249c15e39cb776be2bd0437588b5d603fde7d8
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-23 15:39:59 +0100

    Update urls.py

--- backend/wallet/urls.py
+++ backend/wallet/urls.py
@@ -1,9 +1,18 @@
 from rest_framework.routers import DefaultRouter
 from .views import WalletViewSet, WalletTransactionViewSet, WalletSecurityViewSet
+from .views import StandingOrderViewSet
 
 router = DefaultRouter()
 router.register(r"wallets", WalletViewSet, basename="wallet")
 router.register(r"transactions", WalletTransactionViewSet, basename="wallet-txn")
 router.register(r"security", WalletSecurityViewSet, basename="wallet-security")
+router.register(r"standing-orders", StandingOrderViewSet, basename="standing-order")
+
 
 urlpatterns = router.urls
+
+
+
+
+
+
