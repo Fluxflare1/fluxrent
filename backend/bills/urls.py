2b3d69f9d78fcaae94c0a2829bb5925834b75a2d
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-24 08:24:53 +0100

    Update urls.py

--- backend/bills/urls.py
+++ backend/bills/urls.py
@@ -1,10 +1,15 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import InvoiceViewSet, BillItemViewSet, PaymentRecordViewSet
 
 router = DefaultRouter()
 router.register(r"invoices", InvoiceViewSet, basename="invoice")
-router.register(r"items", BillItemViewSet, basename="billitem")
 router.register(r"payments", PaymentRecordViewSet, basename="payment")
+router.register(r"items", BillItemViewSet, basename="billitem")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+
 
