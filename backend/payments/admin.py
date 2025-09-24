5f7e16b94ca73df2287de0f43788c8acf23ceda2
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-24 17:31:22 +0100

    Create admin.py

---
+++ backend/payments/admin.py
@@ -0,0 +1,9 @@
+# backend/payments/admin.py
+from django.contrib import admin
+from .models import PaymentRecord
+
+@admin.register(PaymentRecord)
+class PaymentRecordAdmin(admin.ModelAdmin):
+    list_display = ("uid", "invoice", "tenant", "amount", "method", "status", "created_at")
+    search_fields = ("uid", "invoice__uid", "tenant__email", "reference")
+    list_filter = ("method", "status", "created_at")
