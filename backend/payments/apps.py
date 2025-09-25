0a9d64f1f3e0978fe46bfbcd7bd6a640a251a9db
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-25 09:38:01 +0100

    Update apps.py

--- backend/payments/apps.py
+++ backend/payments/apps.py
@@ -10,10 +10,3 @@ class PaymentsConfig(AppConfig):
         # import signals so they are registered
         from . import signals  # noqa: F401
 
-
-class PropertiesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "properties"
-
-    def ready(self):
-        import properties.signals
