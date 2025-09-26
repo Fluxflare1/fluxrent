53fc3bbc751505846de0855d8f4655dc48b8f01f
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-26 13:56:21 +0100

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { AuthProvider } from "@/components/AuthProvider";
 
+
 export const metadata = {
   title: "FluxRent",
   description: "Tenant, Property management platform",
