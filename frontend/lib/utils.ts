4866075b47074d687cb42661dea4322a6469248c
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-30 20:26:48 +0100

    Update utils.ts

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,13 +1,7 @@
+// lib/utils.ts
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: any[]) {
   return twMerge(clsx(inputs));
-}
-
-
-
-// frontend/lib/utils.ts
-export function cn(...classes: (string | boolean | null | undefined)[]) {
-  return classes.filter(Boolean).join(" ");
 }
