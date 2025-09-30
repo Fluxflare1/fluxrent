893d155209f522b6fce1d096c71c2f1171e28731
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-30 14:46:55 +0100

    Create apiOwner.ts

---
+++ frontend/lib/apiOwner.ts
@@ -0,0 +1,26 @@
+// frontend/lib/apiOwner.ts
+import api from "./api";
+
+export async function fetchOwnerSummary() {
+  return api.get("/owner/dashboard/summary/").then(res => res.data);
+}
+
+export async function fetchRevenueBreakdown() {
+  return api.get("/owner/dashboard/revenue_breakdown/").then(res => res.data);
+}
+
+export async function fetchAllUsers() {
+  return api.get("/owner/users/").then(res => res.data);
+}
+
+export async function suspendUser(id: number) {
+  return api.post(`/owner/users/${id}/suspend/`).then(res => res.data);
+}
+
+export async function verifyKYC(id: number) {
+  return api.post(`/owner/users/${id}/verify_kyc/`).then(res => res.data);
+}
+
+export async function fetchBoostedProperties() {
+  return api.get("/owner/properties/").then(res => res.data);
+}
