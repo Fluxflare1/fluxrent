2e0f881a9b11f704dd9d28f86cc47caef9d00cff
Itebimien Momodu <fluxflareltd@gmail.com>
2025-09-30 10:57:19 +0100

    Create page.tsx

---
+++ frontend/app/listings/[id]/page.tsx
@@ -0,0 +1,4 @@
+// inside listing detail (client component)
+import BoostButton from "@/components/listings/BoostButton";
+
+<BoostButton listingId={listing.id} onBoosted={() => { /* refetch listing or update UI */ }} />
