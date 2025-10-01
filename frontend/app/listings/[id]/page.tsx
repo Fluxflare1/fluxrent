b8a45894631f5360dcf2f36fb67aa63e00bbba99
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-01 11:35:54 +0100

    Delete frontend/app/listings/[id] directory

--- frontend/app/listings/[id]/page.tsx
+++
@@ -1,4 +0,0 @@
-// inside listing detail (client component)
-import BoostButton from "@/components/listings/BoostButton";
-
-<BoostButton listingId={listing.id} onBoosted={() => { /* refetch listing or update UI */ }} />
