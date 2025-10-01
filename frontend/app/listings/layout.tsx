777ba4a0a0241f47f80f1c9acc48e6a846d31199
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-01 12:24:25 +0100

    Update layout.tsx

--- frontend/app/listings/layout.tsx
+++ frontend/app/listings/layout.tsx
@@ -1,5 +1,5 @@
 // app/listings/layout.tsx
-import "../globals.css";
+import "../../styles/globals.css";
 import { ReactNode } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
