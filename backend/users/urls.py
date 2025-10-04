1faa6b012ba817ad9d510ab46929808a2cf6e966
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-04 11:29:11 +0100

    Update urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
+# backend/users/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ObtainTokenPairView
+from .views import UserViewSet, request_access, ObtainTokenPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 router = DefaultRouter()
@@ -10,4 +11,11 @@ urlpatterns = [
     path("auth/token/", ObtainTokenPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", include(router.urls)),
+    path("users/request-access/", request_access, name="request_access"),
 ]
+
+
+
+
+
+
